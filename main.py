fddd59c054411846021c492dac9b80c461657517
Aleks B <aleksandir@me.com>
2023-11-11 20:54:02 +1030

    Add 3x3 grid with buttons to main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("Hello World!")
+# Create a 3x3 grid
+grid = [[None for _ in range(3)] for _ in range(3)]
+
+# Loop through the grid
+for i in range(3):
+    cols = st.beta_columns(3)
+    for j in range(3):
+        # Create a button in each cell
+        grid[i][j] = cols[j].button(f"Button {i*3 + j + 1}")
