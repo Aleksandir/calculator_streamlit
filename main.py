c070e620ff470f838dd7815397eb634f51fc09e8
Aleks B <aleksandir@me.com>
2023-11-11 20:56:10 +1030

    Replace st.beta_columns with st.columns.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ grid = [[None for _ in range(3)] for _ in range(3)]
 
 # Loop through the grid
 for i in range(3):
-    cols = st.beta_columns(3)
+    cols = st.columns(3)
     for j in range(3):
         # Create a button in each cell
         grid[i][j] = cols[j].button(f"Button {i*3 + j + 1}")
