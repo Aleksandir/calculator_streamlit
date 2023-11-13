7bb375cd61a5b182fd2d0088eee9c24f4304b6e4
Aleks B <aleksandir@me.com>
2023-11-13 17:48:19 +1030

    Add unit tests for evaluate_calculation function in main.py

---
+++ test_main.py
@@ -0,0 +1,28 @@
+# Description: Unit tests for main.py
+
+# import re
+
+import streamlit as st
+
+from main import evaluate_calculation
+
+
+def test_evaluate_calculation():
+    assert evaluate_calculation("") == ""
+    assert evaluate_calculation("1+2") == "3"
+    assert evaluate_calculation("2*3") == "6"
+    assert evaluate_calculation("4/2") == "2"
+    assert evaluate_calculation("5-3") == "2"
+    assert evaluate_calculation("2*(3+4)") == "14"
+    assert evaluate_calculation("(3+4)*2") == "14"
+    assert evaluate_calculation("1/0") == "Error"
+    assert evaluate_calculation("1 + 2 * 3") == "7"
+    assert evaluate_calculation("(1 + 2) * 3") == "9"
+    assert evaluate_calculation("1 + (2 * 3)") == "7"
+    assert evaluate_calculation("1 + (2 * 3))") == "Error"
+    assert evaluate_calculation("1 + (2 * 3)") == "7"
+    assert evaluate_calculation("1 + (2 * 3))") == "Error"
+
+
+if __name__ == "__main__":
+    test_evaluate_calculation()
